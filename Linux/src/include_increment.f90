ca8def7653bd93c89a2813693589607058d84e94
Sven Berendsen <sberendsen@gmail.com>
2026-04-04 00:37:08 +0100

    removed the linux-only version as the cmake one (with IFX only currently) works

--- Linux/src/include_increment.f90
+++
@@ -1,7 +0,0 @@
-!code inserted in INCLUDE_* functions
-INTEGER, INTENT(IN)            :: n       !increment
-INTEGER                        :: sz
-IF(ASSOCIATED(s)) THEN ; sz=SIZE(s) ; old=>s ; NULLIFY(s) ; ELSE ; sz=0 ; ENDIF
-ALLOCATE(s(sz+n))
-IF(sz>0) THEN ; s(1:sz)=old ; DEALLOCATE(old) ; ENDIF
-!IF(PRESENT(default)) s(sz+1:) = default
